ba4868d5f0951152bb5565d5b9b4f2534c363f4b
hfagerlund <heini@bytesofdesign.com>
2018-04-15 13:30:49 -0400

    #10: Remove (variable) duplication in tests
    * adds and disallows overwriting of global variables

--- __tests__/bar.test.js
+++ __tests__/bar.test.js
@@ -5,39 +5,8 @@ import { assert } from 'chai';
 import { shallow } from 'enzyme';
 
 describe('Bar component', () => {
-  const ridingResult = [
-    {
-      name: 'Henry Spurr',
-      partyId: 13,
-      votes: 834,
-      isElected: false,
-      partyCode: 'NDP'
-    },
-    {
-      name: 'Stephen McNeil',
-      partyId: 12,
-      votes: 7709,
-      isElected: true,
-      partyCode: 'LIB'
-    },
-    {
-      name: 'Ginny Hurlock',
-      partyId: 14,
-      votes: 1390,
-      isElected: false,
-      partyCode: 'PC'
-    },
-    {
-      name: 'Ron Neufeld',
-      partyId: 11,
-      votes: 227,
-      isElected: false,
-      partyCode: 'GRN'
-    }
-  ];
-
   const BAR_PROPS = {
-    ridingResults: ridingResult
+    ridingResults: testRiding.results
   };
 
   describe('rendering tests', () => {
--- __tests__/textsummary.test.js
+++ __tests__/textsummary.test.js
@@ -10,43 +10,6 @@ test('TextSummary component renders a riding summary', () => {
 });
 
 test('TextSummary component matches its snapshot with riding data', () => {
-  const ridings = {
-    id: 1,
-    name: 'Annapolis',
-    num: 1,
-    pollsReported: 57,
-    pollsTotal: 57,
-    results: [
-      {
-        name: 'Henry Spurr',
-        partyId: 13,
-        votes: 834,
-        isElected: false,
-        partyCode: 'NDP'
-      },
-      {
-        name: 'Stephen McNeil',
-        partyId: 12,
-        votes: 7709,
-        isElected: true,
-        partyCode: 'LIB'
-      },
-      {
-        name: 'Ginny Hurlock',
-        partyId: 14,
-        votes: 1390,
-        isElected: false,
-        partyCode: 'PC'
-      },
-      {
-        name: 'Ron Neufeld',
-        partyId: 11,
-        votes: 227,
-        isElected: false,
-        partyCode: 'GRN'
-      }
-    ]
-  };
-  const tree = renderer.create(<TextSummary riding={ridings} />);
+  const tree = renderer.create(<TextSummary riding={testRiding} />);
   expect(tree).toMatchSnapshot();
 });
